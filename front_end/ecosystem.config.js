f06da27751920001eba4c86e7ab596f975846f8c
shize <a1844323@student.adelaide.edu.au>
2024-07-24 11:00:32 +0930

    update ecosystem

--- front_end/ecosystem.config.js
+++ front_end/ecosystem.config.js
@@ -1,14 +1,13 @@
 module.exports = {
-    apps: [
-      {
-        name: "front-end-app",
-        script: "npm",
-        args: "start",
-        env: {
-          NODE_ENV: "production",
-          PORT: 3000 
-        }
-      }
-    ]
-  };
-  
+  apps: [
+    {
+      name: 'front_end',
+      script: 'node_modules/react-scripts/scripts/start.js',
+      cwd: '/root/Kasta/front_end',
+      watch: true,
+      env: {
+        NODE_ENV: 'development',
+      },
+    },
+  ],
+};
