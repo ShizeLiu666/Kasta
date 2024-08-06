6657f973903267683f1ab879fd756a6abf6a912f
Jack Liu <jackliu@haneco.local>
2024-08-06 17:00:26 +0930

    change environment to production

--- back_end/database.js
+++ back_end/database.js
@@ -4,7 +4,7 @@ const mongoURI_development = 'mongodb://localhost:27017';
 const production_development = 'mongodb://admin:kasta_31July@174.138.109.122:27017/kasta?authSource=admin';
 
 // 连接到数据库
-const mainDB = mongoose.createConnection(mongoURI_development, {
+const mainDB = mongoose.createConnection(production_development, {
   // useNewUrlParser: true,
   // useUnifiedTopology: true
 });
